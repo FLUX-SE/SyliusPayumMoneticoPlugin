db69091eb2f4a9b763ff5b0ac83fac493d52aeb3
Francis Hilaire <Prometee@users.noreply.github.com>
2018-08-07 18:03:57 +0200

    Add constraints

--- src/Form/Type/MoneticoGatewayConfigurationType.php
+++ src/Form/Type/MoneticoGatewayConfigurationType.php
@@ -6,10 +6,12 @@ use Ekyna\Component\Payum\Monetico\Api\Api;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
 
 final class MoneticoGatewayConfigurationType extends AbstractType
 {
@@ -20,31 +22,38 @@ final class MoneticoGatewayConfigurationType extends AbstractType
     {
         $builder
             ->add('mode', ChoiceType::class, [
+                'expanded' => true,
+                'label' => 'prometee.monetico.fields.mode.label',
                 'choices' => [
                     'prometee.monetico.mode.production' => Api::MODE_PRODUCTION,
                     'prometee.monetico.mode.test' => Api::MODE_TEST
                 ],
-                'label' => 'prometee.monetico.fields.mode',
             ])
             ->add('bank', ChoiceType::class, [
+                'label' => 'prometee.monetico.fields.bank.label',
                 'choices' => [
-                    'prometee.monetico.banque.cic' => Api::BANK_CIC,
-                    'prometee.monetico.banque.cm' => Api::BANK_CM,
-                    'prometee.monetico.banque.obc' => Api::BANK_OBC
+                    'prometee.monetico.bank.cic' => Api::BANK_CIC,
+                    'prometee.monetico.bank.cm' => Api::BANK_CM,
+                    'prometee.monetico.bank.obc' => Api::BANK_OBC
                 ],
-                'label' => 'prometee.monetico.fields.bank',
             ])
-            ->add('tpe', TextType::class, [
-                'label' => 'prometee.monetico.fields.tpe',
+            ->add('tpe', IntegerType::class, [
+                'label' => 'prometee.monetico.fields.tpe.label',
+                'help' => 'prometee.monetico.fields.tpe.help',
                 'constraints' => [
                     new NotBlank([
                         'message' => 'prometee.monetico.tpe.not_blank',
                         'groups' => ['sylius']
-                    ])
+                    ]),
+                    new Length([
+                        'groups' => ['sylius'],
+                        'min' => 7,
+                        'max' => 7
+                    ]),
                 ],
             ])
             ->add('key', TextType::class, [
-                'label' => 'prometee.monetico.fields.key',
+                'label' => 'prometee.monetico.fields.key.label',
                 'constraints' => [
                     new NotBlank([
                         'message' => 'prometee.monetico.key.not_blank',
@@ -53,11 +62,16 @@ final class MoneticoGatewayConfigurationType extends AbstractType
                 ],
             ])
             ->add('company', TextType::class, [
-                'label' => 'prometee.monetico.fields.company',
+                'label' => 'prometee.monetico.fields.company.label',
+                'help' => 'prometee.monetico.fields.company.help',
                 'constraints' => [
                     new NotBlank([
                         'message' => 'prometee.monetico.company.not_blank',
                         'groups' => ['sylius']
+                    ]),
+                    new Length([
+                        'groups' => ['sylius'],
+                        'max' => 20
                     ])
                 ],
             ])
