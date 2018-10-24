29768e3f56d430164f3135ddc170e4d1de92ae64
Francis Hilaire <Prometee@users.noreply.github.com>
2018-10-24 16:01:45 +0200

    Rename action

--- src/Controller/NotifyController.php
+++ src/Controller/NotifyController.php
@@ -12,22 +12,21 @@ namespace Prometee\SyliusPayumMoneticoPlugin\Controller;
 use Doctrine\ORM\NonUniqueResultException;
 use Doctrine\ORM\NoResultException;
 use Ekyna\Component\Payum\Monetico\Api\Api;
-use Payum\Core\Request\GetHumanStatus;
+use Payum\Bundle\PayumBundle\Controller\PayumController;
 use Payum\Core\Request\Notify;
 use Sylius\Component\Core\Model\PaymentInterface;
 use Sylius\Component\Core\Model\PaymentMethodInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-class NotifyController extends Controller
+class NotifyController extends PayumController
 {
     /**
      * @param Request $request
      * @return Response
      */
-    public function index(Request $request) {
+    public function doAction(Request $request) {
         // Get the reference you set in your ConvertAction
         if (null === $reference = $request->request->get('reference')) {
             throw new NotFoundHttpException();
@@ -64,12 +63,9 @@ class NotifyController extends Controller
         $payment_method = $payment->getMethod();
         $gateway_name = $payment_method->getGatewayConfig()->getGatewayName();
 
-        $payum = $this->get('payum');
-
         // Execute notify & status actions.
-        $gateway = $payum->getGateway($gateway_name);
+        $gateway = $this->getPayum()->getGateway($gateway_name);
         $gateway->execute(new Notify($payment));
-        $gateway->execute(new GetHumanStatus($payment));
 
         // We don't invalidate payment tokens because if the customer click on go back to the store
         // the token will not exists anymore so there will be a 404 error page
